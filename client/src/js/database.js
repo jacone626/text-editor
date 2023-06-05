bb969695c0f5db5ecbfb561e516c359fca8a06e0
James A Cone <jacone626@gmail.com>
2023-06-05 14:22:21 -0400

    updated datbase.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,21 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   
   console.log('Put to the database');
 
   // Create a connection to the database database and version we want to use.
-  const todosDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .put() method on the store and pass in the content.
-  const request = store.put({ id: id, todo: content });
+  const request = store.put({ id: id, content: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -40,10 +40,10 @@ export const getDb = async () => {
 console.log('GET all from the database');
 
 // Create a connection to the database database and version we want to use.
-const todosDb = await openDB('jate', 1);
+const jateDb = await openDB('jate', 1);
 
 // Create a new transaction and specify the database and data privileges.
-const tx = todosDb.transaction('jate', 'readonly');
+const tx = jateDb.transaction('jate', 'readonly');
 
 // Open up the desired object store.
 const store = tx.objectStore('jate');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,7 +35,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'JATE Text Editor',
-        short_name: 'Text Editor',
+        short_name: 'JATE',
         description: 'Add text to this app!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
